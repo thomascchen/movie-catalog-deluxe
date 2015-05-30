1c95da9766d3072413da065b7833af149abeb8d9
Thomas Chen <chen.thomas.c@gmail.com>
2015-05-30 12:46:14 -0400

    Delete JS files and fix dynamic view routes
    - Delete unused Foundation Javascript files from public folder
    - Fixed dynamic view routes to reference unique primary key id's for actors and movies instead of actor names and movie titles

--- server.rb
+++ server.rb
@@ -11,36 +11,42 @@ def db_connection
   end
 end
 
+get '/' do
+  redirect '/movies'
+end
+
 get '/actors' do
-  actors = db_connection { |conn| conn.exec("SELECT name FROM actors ORDER BY name")}
+  actors = db_connection { |conn| conn.exec("SELECT id, name FROM actors ORDER BY name")}
   erb :'actors/index', locals: { actors: actors }
 end
 
 get '/actors/:id' do
-  actor_info = db_connection { |conn| conn.exec_params("SELECT actors.name AS actor, movies.title AS movie, cast_members.character AS role
+  actor = db_connection { |conn| conn.exec("SELECT name FROM actors WHERE id = $1", [params[:id]] )}
+  actor_info = db_connection { |conn| conn.exec("SELECT actors.name AS actor, movies.id AS movie_id, movies.title AS movie, cast_members.character AS role
     FROM cast_members
     JOIN movies ON cast_members.movie_id = movies.id
     JOIN actors ON cast_members.actor_id = actors.id
-    WHERE actors.name = $1", [params[:id]] )}
-  erb :'actors/show', locals: { actor: params[:id], actor_info: actor_info }
+    WHERE actors.id = $1", [params[:id]] )}
+  erb :'actors/show', locals: { actor: actor, actor_info: actor_info }
 end
 
 get '/movies' do
-  movies = db_connection { |conn| conn.exec("SELECT movies.title AS movie, movies.year AS year, movies.rating AS rating, genres.name AS genre, studios.name AS studio
+  movies = db_connection { |conn| conn.exec("SELECT movies.id AS id, movies.title AS movie, movies.year AS year, movies.rating AS rating, genres.name AS genre, studios.name AS studio
     FROM movies
     LEFT JOIN genres ON movies.genre_id = genres.id
     LEFT JOIN studios ON movies.studio_id = studios.id
-    ORDER BY movies.title;")}
+    ORDER BY movies.title")}
   erb :'movies/index', locals: { movies: movies }
 end
 
 get '/movies/:id' do
-  movie_info = db_connection { |conn| conn.exec("SELECT movies.title AS movie, movies.year AS year, movies.rating AS rating, genres.name AS genre, studios.name AS studio, actors.name AS actor, cast_members.character AS role, movies.synopsis AS synopsis
+  movie = db_connection { |conn| conn.exec("SELECT id, title FROM movies WHERE id = $1", [params[:id]] )}
+  movie_info = db_connection { |conn| conn.exec("SELECT movies.title AS movie, movies.year AS year, movies.rating AS rating, genres.name AS genre, studios.name AS studio, actors.id AS actor_id, actors.name AS actor, cast_members.character AS role, movies.synopsis AS synopsis
     FROM movies
     LEFT JOIN cast_members ON movies.id = cast_members.movie_id
     LEFT JOIN genres ON movies.genre_id = genres.id
     LEFT JOIN studios ON movies.studio_id = studios.id
     LEFT JOIN actors ON cast_members.actor_id = actors.id
-    WHERE movies.title = $1", [params[:id]] )}
-  erb :'movies/show', locals: { movie: params[:id], movie_info: movie_info }
+    WHERE movies.id = $1", [params[:id]] )}
+  erb :'movies/show', locals: { movie: movie, movie_info: movie_info }
 end
